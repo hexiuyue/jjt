00a585420461ab655349e952013b7c05f0fb023f
何秀月 <2040777805@qq.com>
2019-06-17 09:11:31 +0800

    增加px2dip方法

--- baselib/src/main/java/com/guoshi/baselib/base/BaseActivity.java
+++ baselib/src/main/java/com/guoshi/baselib/base/BaseActivity.java
@@ -37,7 +37,13 @@ public class BaseActivity extends FragmentActivity {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (dpValue * scale + 0.5f);
     }
-
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
     public static int getstatusbar(Context context){
         int result = 0;
         int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
